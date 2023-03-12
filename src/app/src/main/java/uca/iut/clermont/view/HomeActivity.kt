48381fee8d6c9beda7e6cd109eab97f280755c4d
Emre <Emre.kartal@etu.uca.fr>
2023-03-12 20:24:39 +0100

    home page done, also add new fonts and glide library :milky_way:

--- src/app/src/main/java/uca/iut/clermont/view/HomeActivity.kt
+++ src/app/src/main/java/uca/iut/clermont/view/HomeActivity.kt
@@ -3,11 +3,15 @@ package uca.iut.clermont.view
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import uca.iut.clermont.R
 
 class HomeActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        Glide.with(this)
+            .load("https://crests.football-data.org/1765.svg")
+            .into(findViewById(R.id.ImageFirstTeam))
     }
 }
