826f1709c5de48af49188b567d4f3e1b3827e985
Emre KARTAL <emre.kartal@etu.uca.fr>
2023-03-13 22:21:30 +0100

    Mise à jour de 'src/app/src/main/java/uca/iut/clermont/view/FavoriteActivity.kt'

--- src/app/src/main/java/uca/iut/clermont/view/FavoriteActivity.kt
+++ src/app/src/main/java/uca/iut/clermont/view/FavoriteActivity.kt
@@ -3,7 +3,7 @@ package uca.iut.clermont.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class FavoriteActivity : AppCompatActivity() {
+class FavoriteActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
